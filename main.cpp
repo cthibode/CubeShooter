70ae9dea11b597bf5787abddafa0ee5e5124297e
cthibode <camish64@sbcglobal.net>
2014-10-22 16:34:12 -0700

    Set up Visual Studio

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+using namespace std;
+
+int main() {
+   printf("hi\n");
+   int i;
+   scanf("%d", &i);
+
+}
---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#ifdef __APPLE__
+#include "GLFW/glfw3.h"
+#include <OPENGL/gl.h>
+#endif
+
+#ifdef __unix__
+#include "GLFW/glfw3.h"
+#endif
+
+#ifdef _WIN32
+#include <GL/glew.h>
+#include "GLFW/glfw3.h"
+
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glfw3dll.lib")
+#endif
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+#endif
